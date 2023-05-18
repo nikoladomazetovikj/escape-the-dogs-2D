5bfb52848cff89902c8c7734e1b61c9dd6574942
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-18 23:25:09 +0200

    fix: move player and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Animator animator;
+    private Rigidbody2D rb;
+    public float runSpeed = 5f;
+    public float jumpForce = 5f;
     
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,16 +27,22 @@ public class PlayerController : MonoBehaviour
         if (horizontalInput > 0)
         {
             animator.SetBool("Run", true);
+            // Move the object horizontally
+            rb.velocity = new Vector2(horizontalInput * runSpeed, rb.velocity.y);
         }
         else
         {
             animator.SetBool("Run", false);
+            // Stop the horizontal movement
+            rb.velocity = new Vector2(0f, rb.velocity.y);
         }
 
         // Jump
         if (jumpInput)
         {
             animator.SetTrigger("Jump");
+            // Add upward force for jumping
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
     }
 }
