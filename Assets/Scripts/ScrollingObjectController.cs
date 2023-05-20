ab39df8bbe5ee6580c092eb8a53d4cca56601378
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-20 23:09:12 +0200

    fix: repeating scene setup

--- Assets/Scripts/ScrollingObjectController.cs
+++ Assets/Scripts/ScrollingObjectController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ScrollingObjectController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private bool isManMoving;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector2(GameController.instance.scrollSpeed, 0);
+        rb2d.velocity = Vector2.zero;
+        isManMoving = false;
     }
 
     // Update is called once per frame
@@ -19,5 +22,35 @@ public class ScrollingObjectController : MonoBehaviour
         {
             rb2d.velocity = Vector2.zero;
         }
+        else
+        {
+            // Check if the player object is moving
+            bool isMoving = GetPlayerMoving();
+            SetManMoving(isMoving);
+        }
+    }
+
+    void SetManMoving(bool isMoving)
+    {
+        isManMoving = isMoving;
+        if (isManMoving)
+        {
+            rb2d.velocity = new Vector2(GameController.instance.scrollSpeed, 0);
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+    }
+
+    bool GetPlayerMoving()
+    {
+        GameObject playerObject = GameObject.FindWithTag("Man");
+        if (playerObject != null)
+        {
+            Rigidbody2D playerRigidbody = playerObject.GetComponent<Rigidbody2D>();
+            return playerRigidbody.velocity.magnitude > 0f;
+        }
+        return false;
     }
 }
