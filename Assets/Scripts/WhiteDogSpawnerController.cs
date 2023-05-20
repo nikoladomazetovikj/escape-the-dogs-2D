854d6a9f91b8e5faa28da0b9829ca5a88309c9cb
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-21 01:56:38 +0200

    feat: white dog spawner

---
+++ Assets/Scripts/WhiteDogSpawnerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteDogSpawnerController : MonoBehaviour
+{
+    public GameObject whiteDogPrefab;
+    public float spawnInterval = 8f;
+    public bool rotatePrefab = true; // Flag to enable rotation
+
+    private void Start()
+    {
+        StartCoroutine(SpawnWhiteDogs());
+    }
+
+    private IEnumerator SpawnWhiteDogs()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            // Instantiate the white dog prefab
+            GameObject whiteDog = Instantiate(whiteDogPrefab, transform.position, Quaternion.identity);
+
+            // Rotate the white dog prefab if enabled
+            if (rotatePrefab)
+            {
+                whiteDog.transform.Rotate(0f, 180f, 0f); // Rotate by 180 degrees along the Y-axis
+            }
+        }
+    }
+}
