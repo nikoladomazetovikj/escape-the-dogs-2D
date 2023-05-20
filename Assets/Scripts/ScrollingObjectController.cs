2881a2255c82476e0512557ce1e3353741de37e7
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-21 00:42:22 +0200

    fix: minor changes

--- Assets/Scripts/ScrollingObjectController.cs
+++ Assets/Scripts/ScrollingObjectController.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class ScrollingObjectController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-    private float backgroundWidth;
-
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = new Vector2(GameController.instance.scrollSpeed, 0);
-
-        // Get the width of the background sprite or collider
-        backgroundWidth = GetBackgroundWidth();
     }
 
     // Update is called once per frame
@@ -23,44 +18,7 @@ public class ScrollingObjectController : MonoBehaviour
         if (GameController.instance.gameOver)
         {
             rb2d.velocity = Vector2.zero;
-        }
-
-        // Check if the background has moved offscreen
-        if (transform.position.x < -backgroundWidth)
-        {
-            // Reset the background position to repeat the scene
-            RepositionBackground();
+            
         }
     }
-
-    float GetBackgroundWidth()
-    {
-        // Get the width of the background sprite renderer or collider
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        if (spriteRenderer != null)
-        {
-            return spriteRenderer.bounds.size.x;
-        }
-
-        Collider2D collider = GetComponent<Collider2D>();
-        if (collider != null)
-        {
-            return collider.bounds.size.x;
-        }
-
-        return 0f;
-    }
-
-    void RepositionBackground()
-    {
-        // Calculate the new position for the background
-        float backgroundWidth = GetBackgroundWidth();
-        Vector2 currentPosition = transform.position;
-        Vector2 newPosition = new Vector2(currentPosition.x + backgroundWidth, currentPosition.y);
-
-        // Move the background to the new position
-        transform.position = newPosition;
-    }
-
-    
 }
