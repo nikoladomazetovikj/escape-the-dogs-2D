5df39d7eb1f086c2a0a17192e42cdff1922726c6
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-21 21:50:28 +0200

    fix: player jumps more realistic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     public float runSpeed = 0.5f;
     public float jumpForce = 2f;
-    public float maxJumpForce = 2.8f; // Maximum jump force limit
+    public float jumpDuration = 0.5f; // Duration of the jump
+
+    private float jumpTime; // Time elapsed since the jump started
+    private bool isJumping; // Flag to track if the player is currently jumping
 
     // Start is called before the first frame update
     void Start()
@@ -39,12 +42,22 @@ public class PlayerController : MonoBehaviour
         }
 
         // Jump
-        if (jumpInput)
+        if (jumpInput && !isJumping)
         {
             animator.SetTrigger("Jump");
-            // Apply upward force for jumping, limited to maxJumpForce
-            rb.AddForce(new Vector2(0f, Mathf.Min(jumpForce, maxJumpForce)), ForceMode2D.Impulse);
+            isJumping = true;
+            jumpTime = 0f;
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
+
+        // Check if the jump duration has elapsed
+        if (isJumping && jumpTime >= jumpDuration)
+        {
+            isJumping = false;
+            rb.velocity = new Vector2(rb.velocity.x, 0f); // Cancel vertical velocity
+        }
+
+        jumpTime += Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -54,6 +67,5 @@ public class PlayerController : MonoBehaviour
             GameController.instance.CollectCoin();
             Destroy(collision.gameObject);
         }
-
     }
 }
